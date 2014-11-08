2b22a7db507ab6b6b81ed6b0909f2367e856c33f
CarterNelms <carternelms@bellsouth.net>
2014-11-08 15:53:12 -0600

    separated DM and player states

--- client/views/games/games.js
+++ client/views/games/games.js
@@ -1,42 +1,40 @@
 /* global Boot, Menu, Play */
-var game;
 
 (function(){
   'use strict';
 
   angular.module('tabletop')
   .controller('GamesCtrl', ['$scope', '$http', '$state', '$stateParams', 'Room', 'User', function($scope, $http, $state, $stateParams, Room, User){
-    game = new Phaser.Game(500, 320, Phaser.CANVAS, 'the-game');
+    var game = new Phaser.Game(500, 320, Phaser.CANVAS, 'the-game');
 
     $scope.email = User.getEmail();
     if ($scope.email) {
-      getRoom();
+      startGame();
     } else {
       $scope.$on('email', function(err, email){
         if(!$scope.email){
           $scope.email = email;
-          getRoom();
+          startGame();
         }
       });
     }
 
-    function getRoom(){
+    function startGame(){
       Room.getRoom($stateParams.gameId).then(function(res){
         $scope.room = res.data.room;
-        //if($scope.room.email === $scope.email)
+
+        var dmStates = {Boot:Boot, Menu:Menu, Play:Play},
+        playerStates = {Boot:Boot, Menu:Menu, Play:Play},
+        states = $scope.room.email === $scope.email ? dmStates : playerStates;
+        console.log(states);
+
+        //game states
+        game.state.add('boot', states.Boot);
+        game.state.add('menu', states.Menu);
+        game.state.add('play', states.Play);
+
+        game.state.start('boot');
       });
     }
-
-
-    //game states
-    game.state.add('boot', Boot);
-    game.state.add('menu', Menu);
-    game.state.add('play', Play);
-
-    game.state.start('boot');
-
-
-
-
   }]);
 })();
